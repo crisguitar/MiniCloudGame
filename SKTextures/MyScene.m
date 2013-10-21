ac929dd59024a165ae60b5157269f7cf61debded
gitcris <imac1@Criss-Main-iMac.local>
2013-10-21 17:17:26 -0500

    cloud actions

--- SKTextures/MyScene.m
+++ SKTextures/MyScene.m
@@ -8,42 +8,101 @@
 
 #import "MyScene.h"
 
+@interface MyScene() {
+}
+@property SKSpriteNode *background;
+@property SKSpriteNode *cloud;
+@property SKAction *jumpAction;
+@end
+
 @implementation MyScene
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
     }
     return self;
 }
 
+-(void)didMoveToView:(SKView *)view
+{
+    [self setupScene];
+}
+
+-(void)setupScene
+{
+    self.physicsWorld.gravity = CGVectorMake(-0.0f, -0.3f);
+    [self generateBackground];
+    [self generateCloud];
+}
+
+-(SKSpriteNode *)spriteWithColor:(UIColor *)bgColor size:(CGSize)size
+{
+    SKSpriteNode *skNode = [SKSpriteNode spriteNodeWithColor:bgColor size:size];
+    SKSpriteNode *noise = [SKSpriteNode spriteNodeWithImageNamed:@"Noise"];
+    [noise setBlendMode:SKBlendModeScreen];
+    [skNode addChild:noise];
+    return skNode;
+    
+}
+
+-(UIColor *)randomBrightColor
+{
+    while (true) {
+        float requiredBrightness = 192.0/255.0;
+        float red = (arc4random() % 255) / 255.0;
+        float green = (arc4random() % 255) / 255.0;
+        float blue = (arc4random() % 255) / 255.0;
+        if (red > requiredBrightness || green > requiredBrightness || blue > requiredBrightness) {
+            UIColor *randomColor = [SKColor colorWithRed:red green:green blue:blue alpha:1.0];
+            return randomColor;
+        }
+    }
+}
+
+-(void)generateBackground
+{
+    [_background removeFromParent];
+    _background = [self spriteWithColor:[self randomBrightColor] size:self.frame.size];
+    _background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    _background.zPosition = -1;
+    [self addChild:_background];
+}
+
+-(void)generateCloud
+{
+    [_cloud removeFromParent];
+    _cloud = [SKSpriteNode spriteNodeWithImageNamed:@"cloud"];
+    _cloud.position = CGPointMake(100, self.frame.size.height - 200);
+    _cloud.zPosition = 2;
+    _cloud.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:_cloud.size.height/2];
+    _cloud.physicsBody.dynamic = YES;
+    _cloud.physicsBody.affectedByGravity = NO;
+    [self addChild:_cloud];
+    [self setupCloudActions];
+}
+
+-(void)setupCloudActions
+{
+    SKAction *up = [SKAction moveByX:0 y:5 duration:0.3];
+    SKAction *down = [SKAction moveByX:0 y:-5 duration:0.3];
+    
+    SKAction *standAction = [SKAction sequence:@[up, down]];
+    
+    [_cloud runAction:[SKAction repeatActionForever:standAction]];
+    
+    SKAction *moveUp = [SKAction moveByX: 0 y: 50.0 duration: 0.4];
+    SKAction *moveDown = [SKAction moveByX:0 y:-50 duration:0.3];
+    
+    _jumpAction = [SKAction sequence:@[moveUp, moveDown]];
+}
+
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
+    SKAction *stretch = [SKAction scaleYTo:1.1 duration:0.3];
+    SKAction *stretchBack = [SKAction scaleYTo:1.0 duration:0.3];
+    [_cloud runAction: [SKAction sequence:@[stretch, stretchBack]]];
+    [_cloud runAction: _jumpAction];
 }
 
 -(void)update:(CFTimeInterval)currentTime {
--- SKTextures/ViewController.m
+++ SKTextures/ViewController.m
@@ -11,21 +11,23 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
+- (void)viewWillLayoutSubviews
 {
-    [super viewDidLoad];
-
+    [super viewWillLayoutSubviews];
+    
     // Configure the view.
     SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
-    
-    // Create and configure the scene.
-    SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
-    scene.scaleMode = SKSceneScaleModeAspectFill;
-    
-    // Present the scene.
-    [skView presentScene:scene];
+    if (!skView.scene) {
+        skView.showsFPS = YES;
+        skView.showsNodeCount = YES;
+        
+        // Create and configure the scene.
+        SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
+        scene.scaleMode = SKSceneScaleModeAspectFill;
+        
+        // Present the scene.
+        [skView presentScene:scene];
+    }
 }
 
 - (BOOL)shouldAutorotate
@@ -35,17 +37,18 @@
 
 - (NSUInteger)supportedInterfaceOrientations
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
+    return UIInterfaceOrientationMaskLandscape;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Release any cached data, images, etc that aren't in use.
+}
+
+-(BOOL)prefersStatusBarHidden
+{
+    return YES;
 }
 
 @end
